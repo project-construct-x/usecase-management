de011cc673abd2e21d0fa0d8f2abe67168866d17
lkrone <lkrone@uni-wuppertal.de>
2026-01-19 08:46:52 +0000

    fixed ports

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,30 +1,34 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
       react(),
       tailwindcss(),
-  ],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    proxy: {
-      // In Development: Proxy API-Calls zum Backend
-      '/api': {
-        target: 'http://backend:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+    ],
+    server: {
+      host: '0.0.0.0',
+      port: 3000,
+      proxy: {
+        // In Development: Proxy API-Calls zum Backend
+        '/api': {
+          target: 'http://backend:8000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
-    },
-    hmr: {
-      host: 'localhost',
-      clientPort: 3001
-    },
-    watch: {
-      usePolling: true
+      hmr: {
+        host: 'localhost',
+        clientPort: parseInt(env.VITE_FRONTEND_PORT)
+      },
+      watch: {
+        usePolling: true
+      }
     }
   }
 })
