5762aaed29c0da1695838dbc8790d1040544cb32
lkrone <lkrone@uni-wuppertal.de>
2026-04-09 11:40:33 +0000

    fixed alembic import

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,7 +1,7 @@
 import os
-from ..app.models import Base
-from ..app.models.models import *
-from ..app.models.users import *
+from app.db import Base
+from app.models.models import *
+from app.models.users import *
 
 from logging.config import fileConfig
 
